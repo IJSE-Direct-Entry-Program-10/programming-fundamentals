ac5511196d978779563dc003a4102ab1bbecc662
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-01-16 15:20:22 +0530

    Update the text animation

--- src/LoopDemo2.java
+++ src/LoopDemo2.java
@@ -9,37 +9,59 @@ public class LoopDemo2 {
 //            Thread.sleep(1000);
 //        }
 
-        String[] colors = {"\u001b[30m", "\u001b[31m", "\u001b[32m", "\u001b[33m", "\u001b[34m"};
-        boolean erase = false;
-
-        for (int i = 0, k = 1; true ; i += k) {
-
-            if (i == something.length()) {
-                erase = true;
-                k = -1;
-            }else if (i == 0){
-                erase = false;
-                k = 1;
-            }
-
-            if (!erase) System.out.print(something.charAt(i));
-            if (erase) System.out.print("\b");
-
-            if (!erase) Thread.sleep(200);
-            else Thread.sleep(100);
+        String[] colors = new String[10];
+        /* Filling with colors */
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = "\u001b[3" + i + "m";
         }
 
-//        for (int i = 0; true ; i++) {
+        boolean erase = false;
+
+        /* Method 1 */
+//        for (int i = 0, k = 1, j = 0; true ; i += k) {
+//
+//            if (i == something.length()) {
+//                erase = true;
+//                k = -1;
+//            }else if (i == 0){
+//                erase = false;
+//                k = 1;
+//            }
+//
+//            if (!erase) System.out.printf("%s%s", colors[++j == colors.length ? j = 0: j], something.charAt(i));
+//            else System.out.print("\b \b");
+//
+//            if (!erase) Thread.sleep(100);
+//            else Thread.sleep(50);
+//        }
+
+        /* Method 2 */
+//        for (int i = 0, j=0; true ; i++) {
 //            if (i == something.length()) {
 //                erase = !erase;
 //                i = 0;
 //            }
 //
-//            if (!erase) System.out.print(something.charAt(i));
-//            if (erase) System.out.print("\b");
+//            if (!erase) System.out.printf("%s%s", colors[++j == colors.length ? j = 0: j], something.charAt(i));
+//            else System.out.print("\b \b");
 //
-//            if (!erase) Thread.sleep(200);
-//            else Thread.sleep(100);
+//            if (!erase) Thread.sleep(100);
+//            else Thread.sleep(50);
 //        }
+
+        /* Method 3 */
+        for (int i = 0, j = 0; true ; i++) {
+            if (i / something.length() == 1) {
+                erase = !erase;
+                i = 0;
+                Thread.sleep(200);
+            }
+
+            if (!erase) System.out.printf("%s%s", colors[++j == colors.length ? j = 0: j], something.charAt(i));
+            else System.out.print("\b \b");
+
+            if (!erase) Thread.sleep(100);
+            else Thread.sleep(50);
+        }
     }
 }
