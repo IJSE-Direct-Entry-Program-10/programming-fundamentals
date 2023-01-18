ff96757f004e396ae0ba998bd1352026b6e0599d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-01-18 11:42:55 +0530

    Update SelectionSort2

--- src/statements/demos/algorithms/SelectionSort2.java
+++ src/statements/demos/algorithms/SelectionSort2.java
@@ -7,7 +7,25 @@ public class SelectionSort2 {
     public static void main(String[] args) {
         var nums = new int[]{10, 25, 30, 0, -25, -4, 15, 20};
 
-        /* Todo: Like Selection Sort, via while and for */
+        int k = 0;
+        while (k < nums.length) {
+            var maxIndex = nums.length - 1 - k;             // 7
+            var max = nums[maxIndex];                   // 20
+
+            var i = maxIndex - 1;
+            while (i >= 0){
+                if (max < nums[i]){
+                    max = nums[i];
+                    maxIndex = i;
+                }
+                i--;
+            }
+
+            var temp = nums[nums.length - 1 - k];
+            nums[nums.length - 1 - k] = max;
+            nums[maxIndex] = temp;
+            k++;
+        }
 
         System.out.println(Arrays.toString(nums));  // [-25,-4,0,10,15,20,25,30]
     }
