2d26e039080c7f79a8e5948f48c72a63862f13c5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-01-18 11:12:00 +0530

    Update MinMax Algo

--- src/statements/demos/algorithms/MinMaxAlgo.java
+++ src/statements/demos/algorithms/MinMaxAlgo.java
@@ -4,14 +4,25 @@ public class MinMaxAlgo {
 
     public static void main(String[] args) {
         var nums = new int[]{10, 25, 30, 0, -25, -4, 15, 20};
-        int min = 0;
+        int min = nums[0];
         int minIndex = 0;
-        int max = 0;
+        int max = nums[0];
         int maxIndex = 0;
 
-        /* Todo: Search min,max values and index via while and for loops */
+        int i = 1;
+        while (i < nums.length){
+            if (min > nums[i]){
+                min = nums[i];
+                minIndex = i;
+            }
+            if (max < nums[i]){
+                max = nums[i];
+                maxIndex = i;
+            }
+            i++;
+        }
 
-        System.out.printf("Min value: %s, Min index: %s \n", min,minIndex);
-        System.out.printf("Max value: %s, Max index: %s \n", max,maxIndex);
+        System.out.printf("Min value: %s, Min index: %s \n", min,minIndex); // -25, 4
+        System.out.printf("Max value: %s, Max index: %s \n", max,maxIndex); // 30, 2
     }
 }
